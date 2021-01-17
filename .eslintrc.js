d00ed84befefca7b3e86e82e6e0c091112d1b41c
wdavidcalsin <david2c142@gmail.con>
2021-01-17 13:17:12 -0500

    refactor: 💡 changed the husky commands

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,17 @@
-// module.exports = {
-//     parser: '@typescript-eslint/parser',
-//     extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
-//     parserOptions: {
-//         ecmaVersion: 2018,
-//         sourceType: 'module',
-//         ecmaFeatures: {
-//             jsx: true
-//         }
-//     },
-//     rules: {},
-//     settings: {
-//         react: {
-//             version: 'detect'
-//         }
-//     }
-// };
-
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true,
-        es6: true
-    },
-    parser: '@typescript-eslint/parser',
+    parser: "@typescript-eslint/parser",
+    extends: ["plugin:@typescript-eslint/recommended", "prettier/@typescript-eslint", "plugin:prettier/recommended"],
     parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        sourceType: 'module'
+        ecmaVersion: 2020,
+        sourceType: "module",
+        // ecmaFeatures: {
+        //     jsx: true
+        // }
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
-        'prettier'
-    ],
-    rules: {
-        'prettier/prettier': 'error',
-        'react/jsx-uses-react': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn'
-    },
-    plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+    rules: {},
     settings: {
         react: {
-            version: 'detect'
-        }
-    }
+            version: "detect",
+        },
+    },
 };
--- src/service/github.service.ts
+++ src/service/github.service.ts
@@ -1,8 +1,8 @@
 class ApiGithub {
-  async getApi() {
-    const response = await fetch("https://api.github.com/users/wdavidcalsin");
-    return await response.json();
-  }
+    async getApi() {
+        const response = await fetch('https://api.github.com/users/wdavidcalsin');
+        return await response.json();
+    }
 }
 
 export default ApiGithub;
