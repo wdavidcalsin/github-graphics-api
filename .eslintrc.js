5284b1cb48b473ec4c60b5d3a9fb87da6a8b7485
wdavidcalsin <david2c142@gmail.con>
2021-01-17 13:14:01 -0500

    refactor: 💡 added settings and changes in format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,53 @@
+// module.exports = {
+//     parser: '@typescript-eslint/parser',
+//     extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+//     parserOptions: {
+//         ecmaVersion: 2018,
+//         sourceType: 'module',
+//         ecmaFeatures: {
+//             jsx: true
+//         }
+//     },
+//     rules: {},
+//     settings: {
+//         react: {
+//             version: 'detect'
+//         }
+//     }
+// };
+
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+        es6: true
     },
-  },
-  settings: {
-    react: {
-      version: "detect",
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        sourceType: 'module'
     },
-  },
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "pluging:@typescript-eslint/recommended",
-  ],
-  rules: {},
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'prettier'
+    ],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn'
+    },
+    plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    }
 };
-// clear;
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,31 @@
-import React from "react";
-import { Bar } from "react-chartjs-2";
-import ApiGithub from "./service/github.service";
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import ApiGithub from './service/github.service';
 
 const data = {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "rgba(255,99,132,0.2)",
-      borderColor: "rgba(255,99,132,1)",
-      borderWidth: 1,
-      hoverBackgroundColor: "rgba(255,99,132,0.4)",
-      hoverBorderColor: "rgba(255,99,132,1)",
-      data: [65, 59, 80, 81, 56, 55, 40],
-    },
-  ],
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'My First dataset',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data: [65, 59, 80, 81, 56, 55, 40],
+        },
+    ],
 };
 
 class App extends React.Component {
-  async componentDidMount() {
-    let res: any = await new ApiGithub().getApi();
-    console.log(res.avatar_url);
-  }
+    async componentDidMount() {
+        const res: any = await new ApiGithub().getApi();
+        console.log(res.avatar_url);
+    }
 
-  render() {
-    return (
-      <Bar
-        data={data}
-        width={100}
-        height={300}
-        options={{ maintainAspectRatio: false }}
-      />
-    );
-  }
+    render() {
+        return <Bar data={data} width={100} height={300} options={{ maintainAspectRatio: false }} />;
+    }
 }
 
 export default App;
